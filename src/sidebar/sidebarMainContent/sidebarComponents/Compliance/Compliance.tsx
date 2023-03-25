cb205de418d7bee2ec8ad71932aa9d53f91732a9
FooKing <skaged@live.co.uk>
2023-03-25 22:34:23 +0000

    Updated Annotations
    Added ability to set or unset all annotations in a btn group

--- src/sidebar/sidebarMainContent/sidebarComponents/Compliance/Compliance.tsx
+++ src/sidebar/sidebarMainContent/sidebarComponents/Compliance/Compliance.tsx
@@ -24,28 +24,28 @@ export default function ComplianceTools() {
   }
 
 
-  function handleAcceptAnnotation() {
+  function handleAnnotationBtnClicked(accept : boolean) {
     const annotationPopup = document.querySelector('.check-notes-popup-wrapper')
     if (annotationPopup) {
       // annotation popup already open just accept and save
-      acceptAnnotations();
+      changeAnnotations(accept);
       return;
     }
     const annotationTab = document.querySelector('#file-menu-notes');
     if (annotationTab) {
       if (annotationTab.classList.contains('toggled')) {
         // we are on the annotations tab so can progress to open the popup
-        openAnnotationPopup();
+        openAnnotationPopup(accept);
       } else {
         // Not on the annotation tab so lets get there.
-        (annotationTab as HTMLElement).click();
+        (annotationTab as HTMLElement)?.click();
         const observer = new MutationObserver((mutations) => {
           for (const mutation of mutations) {
             if (mutation.type === 'childList') {
               const checkNotes = document.querySelector('.check-notes');
               if (checkNotes) {
                 // we are now on the annotations tab lets open the popup and proceed
-                openAnnotationPopup();
+                openAnnotationPopup(accept);
                 observer.disconnect();
                 break;
               }
@@ -58,17 +58,17 @@ export default function ComplianceTools() {
         });
       }
     }
-    function openAnnotationPopup() {
+    function openAnnotationPopup(accept:boolean) {
       const annotationPopupBtn = document.querySelector('.check-notes');
       if (annotationPopupBtn) {
-        (annotationPopupBtn as HTMLElement).click();
+        (annotationPopupBtn as HTMLElement)?.click();
         const observer = new MutationObserver((mutations) => {
           for (const mutation of mutations) {
             if (mutation.type === 'childList') {
               const checkPopup = document.querySelector('.check-notes-popup-wrapper');
               if (checkPopup) {
                 // we are now on the annotations tab lets open the popup and proceed
-                acceptAnnotations();
+                changeAnnotations(accept);
                 observer.disconnect();
                 break;
               }
@@ -81,23 +81,32 @@ export default function ComplianceTools() {
         });
       }
     }
-    function acceptAnnotations() {
+    function changeAnnotations(accept:boolean) {
       const tabMenuItems = document.querySelectorAll('.tabs-menu-item > a');
       tabMenuItems.forEach((t) => {
         if (t) {
-          (t as HTMLElement).click();
+          (t as HTMLElement)?.click();
         }
         const checkboxes = document.querySelectorAll('.check-notes-table input[type="checkbox"]');
         checkboxes.forEach((i) => {
-          if (!(i as HTMLInputElement).checked) {
-            (i as HTMLElement).click();
+          const inputElement = i as HTMLInputElement;
+          if (accept) {
+            if (!inputElement?.checked) {
+              (i as HTMLElement)?.click();
+            }
           }
+          else {
+            if (inputElement?.checked) {
+              (i as HTMLElement)?.click();
+            }
+          }
+
         });
       });
 
       const saveButton = document.querySelector('.save')
       if (saveButton) {
-        (saveButton as HTMLElement).click();
+        (saveButton as HTMLElement)?.click();
       }
     };
   }
@@ -107,7 +116,11 @@ export default function ComplianceTools() {
 
   return (
     <div className="complianceContainer">
-      <button className="btn btn-sm btn-wide btn-primary" onClick={handleAcceptAnnotation}>Accept All Annotations</button>
+      <div className={"btn-group btn-group-horizontal"}>
+        <button className="btn btn-sm btn-primary" onClick={() => handleAnnotationBtnClicked(true)}>☑ Annotations</button>
+        <button className="btn btn-sm btn-primary" onClick={() => handleAnnotationBtnClicked(false)}>☐ Annotations</button>
+      </div>
+
       <button className="btn btn-sm btn-wide btn-primary" onClick={handleCorniceLoad}>Show Cornice</button>
       <button className="btn btn-sm btn-wide btn-primary" onClick={handlePelmetLoad}>Show Pelmet</button>
       <button className="btn btn-sm btn-wide btn-primary" onClick={handlePlinthLoad}>Show Plinth</button>
