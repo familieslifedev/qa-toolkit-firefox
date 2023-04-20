9fa45a8b200b217810d7bce8697033d9f2bd6b3d
Ben Minnikin <ben.minnikin@wrenkitchens.com>
2023-04-20 17:05:29 +0100

    Improved JsonViewer types

--- src/sidebar/sidebarMainContent/sidebarComponents/jsonTools/JsonEditor/JsonViewer.tsx
+++ src/sidebar/sidebarMainContent/sidebarComponents/jsonTools/JsonEditor/JsonViewer.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import { copyFromClipboard, get2DJson, get3DJson, load2DJson, prettyPrintJson, writeToClipboard } from "~Utils/Utils";
 
-const JsonViewer = ({ edit }): JSX.Element => {
+interface JsonViewProps {
+  edit: boolean
+};
+
+const JsonViewer = ({ edit }: JsonViewProps): JSX.Element => {
   const [jsonData, setJsonData] = useState<string | object>(null);
   const [updatedJson, setUpdatedJson] = useState<string | object>(null);
 
@@ -9,9 +13,9 @@ const JsonViewer = ({ edit }): JSX.Element => {
   const handleGet3dJson = async (): Promise<void> => await handleGetJson(get3DJson);
   const handleFromClipboard = async (): Promise<void> => await handleGetJson(copyFromClipboard, true);
 
-  const handleGetJson = async (jsonFunc: Function, parseResult: boolean = false): Promise<void> => {
+  const handleGetJson = async (getJsonCallback: () => Promise<string | object>, parseResult: boolean = false): Promise<void> => {
     try {
-      let result: string | object = await jsonFunc();
+      let result: string | object = await getJsonCallback();
       result = parseResult ? JSON.parse(result as string) : result;
 
       if (!result) return;
@@ -20,7 +24,7 @@ const JsonViewer = ({ edit }): JSX.Element => {
       setUpdatedJson(null);
 
     } catch (err) {
-      setJsonData("Invalid JSON");
+      setJsonData(`Invalid JSON: ${err.message}`);
     }
   }
 
@@ -28,20 +32,19 @@ const JsonViewer = ({ edit }): JSX.Element => {
     const jsonToValidate: string | object = updatedJson ?? jsonData;
 
     try {
-      if (jsonToValidate) {
-        // where should this be used?
-        const parsedJson = JSON.parse(typeof jsonToValidate === 'string' ? jsonToValidate : JSON.stringify(jsonToValidate));
-
-        alert('JSON is valid!');
-      } else {
+      if (!jsonToValidate) {
         alert('Please enter some JSON first');
+        return;
       }
-    } catch (error) {
-      alert('Invalid JSON');
+
+      alert('JSON is valid!');
+
+    } catch (err) {
+      alert(`Invalid JSON: ${err.message}`);
     }
   }
 
-  const handleEditJson = (e: any): void => {
+  const handleEditJson = (e: React.FormEvent): void => {
     const element = e.target as HTMLPreElement;
 
     const newJsonData = element.innerText;
@@ -67,14 +70,15 @@ const JsonViewer = ({ edit }): JSX.Element => {
           <button title="Get Json from feeder link or a direct json" className="btn btn-sm" onClick={handleFromClipboard}>From Clipboard</button>
         </div>
       </div>
+
       <div className="jsonViewerContent mockup-code">
         {jsonData && (
           <pre spellCheck={false} contentEditable={edit} dangerouslySetInnerHTML={{ __html: prettyPrintJson.toHtml(jsonData) }}
-               onInput={handleEditJson} >
+               onInput={handleEditJson}>
           </pre>
-
         )}
       </div>
+
       <div className="jsonReplacerHeader">
         <div className="btn-group">
           <button title="Validate the Json to ensure the integrity" className="btn btn-sm" onClick={handleJsonValidate}>Validate</button>
