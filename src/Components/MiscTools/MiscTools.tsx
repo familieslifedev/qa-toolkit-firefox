bfd9769aa7490ba272bf6b0b7c896976ad616d0a
Chris Scarratt <chris.scarratt@wrenkitchens.com>
2023-05-29 13:47:57 +0100

    Update
    Completed copy to clipboard, if copy fails alert shows to warn user and pastes plan link in console as backup

--- src/Components/MiscTools/MiscTools.tsx
+++ src/Components/MiscTools/MiscTools.tsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react';
 import type { Plan } from "~Utils/planInterface";
-import { copyFromClipboard, load2DJson } from "~Utils/Utils";
+import { copyFromClipboard, get2DJson, load2DJson, writeToClipboard } from "~Utils/Utils";
+import { sendJsonToFeeder } from "~Utils/JsonHelper";
 
 export default function MiscTools() {
 
 
-
 	const autoLayoutTriggered = () => {
 		const observer = new MutationObserver((mutationsList, observer) => {
-
+			const LayoutMenuTab:HTMLLIElement | null = document.querySelector('li.layout-step');
+			const LayoutMenuTabLink:HTMLAnchorElement | null = LayoutMenuTab ? LayoutMenuTab.querySelector('a') : null;
 			const generateBTN:HTMLButtonElement | null = document.querySelector('button.sc-fzXfMC.eoeHYr');
+
+			if (LayoutMenuTabLink && !LayoutMenuTabLink.classList.contains("disabled")) {
+				LayoutMenuTabLink.click();
+			}
 
 			if (generateBTN) {
 				generateBTN.click();
@@ -18,36 +23,50 @@ export default function MiscTools() {
 			}
 		});
 
-		// Start observing the document with the configured parameters
 		observer.observe(document, { childList: true, subtree: true });
 	};
 
 
 
 	const autoGeneratingLayoutDetectCompletion = () => {
-		const observer = new MutationObserver((mutationsList, observer) => {
+		const observer = new MutationObserver(async (mutationsList, observer) => {
 			const splashScreen: HTMLDivElement | null = document.querySelector('.splash-screen');
 
 			if (splashScreen) {
 				const splashScreenChild: HTMLDivElement | null = splashScreen.querySelector('#kitchen-layout-splash-screen');
 
 				if (!splashScreenChild) {
-
 					observer.disconnect();
+						await copyJsonAsFeeder();
+
+
 				}
 			}
 		});
 
-		// Start observing the document with the configured parameters
-		observer.observe(document, { childList: true, subtree: true });
+		observer.observe(document, { childList: true, subtree: true});
 	}
 
+	const copyJsonAsFeeder = async () => {
 
+		get2DJson().then(async (res) => {
+			let currentUrl = window.location.href;
+			const urlText = await sendJsonToFeeder(res, res.plan.planId, currentUrl);
+			if (urlText){
+				console.log(`%cAutolayout Save Json: ${urlText}`, 'color: blue; font-size: medium');
+				try {
+					await writeToClipboard(urlText);
+				}
+				catch (e) {
+					alert(`Failed to copy to clipboard ${e} /n Link available in console`);
+				}
 
+			}
+		})
 
+	}
 
-
-		// This checks on extension mount if the auto-layout generation process has been triggered
+		// This checks on extension mount if the auto-layout generation process has been triggered using a stored chrome storage key to track the state
 		useEffect(() => {
 			const checkAutoPlanLoadTriggered = () => {
 				chrome.storage.local.get(["autoPlanLoadTriggered"], (result) => {
@@ -59,29 +78,25 @@ export default function MiscTools() {
 					}
 				});
 			};
-
 			checkAutoPlanLoadTriggered();
 		}, []);
 
 
-
-
-		const handleAutolayoutGenInitial = async () => {
-			const jsonToLoad = copyFromClipboard().then((res) => {
+		const handleAutolayoutButton = () => {
+			 copyFromClipboard().then((res) => {
 				fetch(res).then((res) => res.json().then((res) => {
 					let planJson:Plan = res;
-					planJson.plan.step = "layout"
+					planJson.plan.step = "base-units"
 					chrome.storage.local.set({ "autoPlanLoadTriggered": true });
 					load2DJson(JSON.stringify(planJson));
 				}));
-				console.log(res);
 			});
 		};
 
 
-		return (
+	return (
 			<div className="jsonContainer">
-				<button className="btn btn-sm btn-wide btn-primary" title="Generates auto-layouts JSONS given a list of plan JSONS" onClick={handleAutolayoutGenInitial}>AutoLayout Gen</button>
+				<button className="btn btn-sm btn-wide btn-primary" title="Generates auto-layouts JSONS given a list of plan JSONS" onClick={handleAutolayoutButton}>AutoLayout Gen</button>
 			</div>
 		);
 	}
